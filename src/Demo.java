7dcc7350d20630b9f0a7d94a12e70c95c0c2a8cc
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2020-11-13 21:38:58 +0700

    connect MongoDB, Description

---
+++ src/Demo.java
@@ -0,0 +1,66 @@
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.bson.Document;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ * Servlet implementation class Demo
+ */
+@WebServlet("/Demo")
+public class Demo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Demo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		MongoClientURI uri = new MongoClientURI(
+		    "mongodb://tiennhm:m1nht13n@cluster0-shard-00-00.brj3o.mongodb.net:27017,cluster0-shard-00-01.brj3o.mongodb.net:27017,cluster0-shard-00-02.brj3o.mongodb.net:27017/X4FIT?ssl=true&replicaSet=atlas-emonwf-shard-0&authSource=admin&retryWrites=true&w=majority");
+		MongoClient mongoClient = new MongoClient(uri);
+		MongoDatabase database = mongoClient.getDatabase("X4FIT");
+		MongoCollection<Document> collection = database.getCollection("USER");
+		/*
+		Document doc = new Document("id", "18110377")
+				.append("name", "Minh Tiến")
+				.append("email", "ngotienhoang09@gmail.com")
+				.append("class", new Document("id", "181101B")
+									.append("major", "Information Technology")
+									.append("faculty", "Faculty of Information Technology"));
+		*/
+		//collection.insertOne(doc);
+		Document myDoc = collection.find().first();
+		response.getWriter().append(myDoc.toJson());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
