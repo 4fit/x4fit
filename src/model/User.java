0ea98abafdc289c62552dc8be0103c4d47f2eaa3
luuvancui <cuiluu@realshare.vn>
2020-12-07 17:42:32 +0700

    finished display data users

--- src/model/User.java
+++ src/model/User.java
@@ -2,9 +2,59 @@ package model;
 
 public class User
 {	
+	private int userId;
 	private String username;
+	private String name;
 	private String password;
 	private String email;
+	private String avata;
+	private int posts_count;
+	
+	public User(int userId, String username, 
+			String name, String pass, String email, 
+			String avata, int posts_count) {
+		this.userId = userId;
+		this.username= username;
+		this.name = name;
+		this.password= pass;
+		this.email= email;
+		this.avata = avata;
+		this.posts_count = posts_count;
+	}
+	
+	public User(String username, String pass, String email ) {
+		this.username= username;
+		this.password= pass;
+		this.email= email;
+	}
+	
+	public int getUserId() {
+		return this.userId;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
+	
+	public String getAvata() {
+		return this.avata;
+	}
+	
+	public void setAvata(String newAvata) {
+		this.avata = newAvata;
+	}
+	
+	public int getPostsCount() {
+		return this.posts_count;
+	}
+	
+	public void setPostsCount(int posts_count) {
+		this.posts_count = posts_count;
+	}
 	
 	public String getUsername() {
 		return username;
@@ -23,19 +73,5 @@ public class User
 	}
 	public void setEmail(String email) {
 		this.email = email;
-	}
-	
-	public User()
-	{
-		this.username= " ";
-		this.password= " ";
-		this.email= " ";
-	}
-	
-	public User(String username, String pass, String email )
-	{
-		this.username= username;
-		this.password= pass;
-		this.email= email;
 	}
 }
