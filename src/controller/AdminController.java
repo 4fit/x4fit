5fefbd6f5e289446e24e8e542e12ac4ccafaa20a
luuvancui <cuiluu@realshare.vn>
2020-12-08 17:43:33 +0700

    giao dien user: add + delete

--- src/controller/AdminController.java
+++ src/controller/AdminController.java
@@ -33,9 +33,8 @@ public class AdminController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String action = request.getServletPath();
-		
 		switch(action) {
-			case "all-users":
+			case "/all-users":
 				getAllUsers(request, response);
 				break;
 		}
