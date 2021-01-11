9b79b297e7a9337a8513bc2794e39c3677520020
luuvancui <cuiluu@realshare.vn>
2021-01-11 11:50:59 +0700

    mod

--- src/model/Category.java
+++ src/model/Category.java
@@ -1,10 +1,12 @@
 package model;
 
-public class Category 
+import org.bson.Document;
+
+public class Category extends Model 
 {
 	private int id;
 	private String name;
-	private String detail;
+	private String description;
 	private int count_post;
 	public int getId() {
 		return id;
@@ -19,10 +21,10 @@ public class Category
 		this.name = name;
 	}
 	public String getDetail() {
-		return detail;
+		return description;
 	}
-	public void setDetail(String detail) {
-		this.detail = detail;
+	public void setDetail(String description) {
+		this.description = description;
 	}
 	public int getCount_post() {
 		return count_post;
@@ -30,5 +32,25 @@ public class Category
 	public void setCount_post(int count_post) {
 		this.count_post = count_post;
 	}
+	
+	public Category(String name, String description) {
+		this.id = Model.getLastestID(CATEGORY) + 1;
+		this.name = name;
+		this.description = description;
+		this.count_post = 0;
+	}
+	
+	public static void Insert(Category category) {
+		Insert(category.getId(), category.getName(), category.getDetail(), category.getCount_post());
+	}
+	
+	public static void Insert(int id, String name, String description, int count_post) {
+		Document doc = new Document("id", id)
+							.append("name", name)
+							.append("description", description)
+							.append("count_post", count_post);
+		Insert(doc, CATEGORY);				
+	}
+	
 	
 }
