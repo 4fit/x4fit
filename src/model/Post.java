51ab440b330c131fe57f0f94f3ae7acf9f8e1622
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-01-11 12:41:01 +0700

    .

--- src/model/Post.java
+++ src/model/Post.java
@@ -341,6 +341,7 @@ public class Post extends Model {
 						Updates.set("updated_at", Utilities.GetCurrentDateTime()),
 						Updates.set("thumbnail_url", thumbnail_url)));
 		return newURL;
+		
 	}
 
 	public void updateVote(String nameField, int idPost, int idUserVote) // Bao gồm upvote, downvote
